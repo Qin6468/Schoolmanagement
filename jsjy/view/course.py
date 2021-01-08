1f12b1e1bd035a6f6c309dcc4d883f7a443de632
Qin6468 <986353358@qq.com>
2021-01-08 11:20:15 +0800

    1/8

--- jsjy/view/course.py
+++ jsjy/view/course.py
@@ -9,14 +9,14 @@ course=Blueprint('course',__name__)
 @course.route('/course')
 def index():
 	return render_template('/course.html')
-@course.route('/course',methods=['POST'])
 
 #获取课程 (1.5修改bug成功能实现数据的获取)
 @course.route('/courselist',methods=['GET'])
 def get_course():
 	perPage=int(request.values.get('perPage'))
 	page=int(request.values.get('page'))
-	c_id=request.values.get('c_id')
+	# id=request.values.get('id')
+	# c_id=request.values.get('c_id')
 	search={}
 	search['name']=request.values.get('name')
 	search['college']=request.values.get('college')
@@ -75,16 +75,54 @@ def add_course():
 		return r({},0,'',{'c_id':'课程已经存在'})
 
 	j_data.setdefault('info','')
-	cl = Course(j_data['c_id'],j_data['name'],j_data['credit'],j_data['college'],j_data['semester_hour'],j_data['number'],j_data['time'],j_data['info'],j_data['local'])
+	cl = Course(j_data['c_id'],j_data['name'],j_data['college'],j_data['credit'],j_data['semester_hour'],j_data['number'],j_data['time'],j_data['local'],j_data['info'])
 	db.session.add(cl)
 	db.session.commit()
 	set_course_count(j_data['c_id'])
 	return r({},0,'添加成功')
 
+#删除课程
+@course.route('/courselist/<int:c_id>',methods=['DELETE'])
+def delete_course(c_id):
+	tc = db.session.query(Course).filter_by(c_id=c_id).first()
+	sql2=db.session.query(Course).filter_by(c_id=c_id).delete()
+	db.session.commit()
+	# set_course_count(tc.c_id);
+	return r({},0,'删除成功')
+
+#修改
+@course.route('/courselist/<int:c_id>',methods=['PUT'])
+def edit_course(c_id):
+	data = request.get_data()
+	j_data =  json.loads(data)
+	
+	tc = db.session.query(Course).filter_by(c_id=c_id).first()
+	# old_class_id=0
+	# flag=False
+	# if tc.class_id != j_data['class_id']:
+	# 	old_class_id=tc.class_id
+	# 	flag=True
+	# 	pass
+	tc.name=j_data['name'],
+	tc.credit=j_data['credit'],
+	tc.college=j_data['college'],
+	tc.semester_hour=j_data['semester_hour'],
+	tc.number=j_data['number'],
+	tc.time=j_data['time'],
+	tc.local=j_data['local'],
+	tc.info=j_data['info'],
+	db.session.commit()
+	# if flag:
+	# 	set_class_count(j_data['class_id']);
+	# 	if old_class_id >0:
+	# 		set_class_count(old_class_id);
+	return r({},0,'修改成功')
+
+#修改课程数（暂时不清楚作用）
 def set_course_count(c_id):
-	if c_id>0:
+	if int(c_id)>0:
 		count=db.session.query(Course).filter_by(c_id=c_id).count()
-		# tc = db.session.query(Class).filter_by(id=c_id).first()
+		tc = db.session.query(Course).filter_by(c_id=c_id).first()
 		# 课程数
 		tc.course_count=count
 		db.session.commit()
@@ -92,9 +130,10 @@ def set_course_count(c_id):
 
 @course.route('/courselist/minlist',methods=['GET'])
 def get_minlist():
-	# data =db.session.query(distinct(Course.college)).all()
-	data=db.session.query(Course.c_id,Course.college).order_by(Course.college.desc()).all()
+	data = db.session.query(distinct(Course.college).label('college')).all()
+	# data=db.session.query(Course.c_id,Course.college).order_by(Course.college.desc()).all()
 	re=[]
 	for x in data:
-		re.append({'label':x[1],'value':x[0]})
+		re.append({'label':x[0],'value':x[0]})
+		# re.append({'label':x[0]})
 	return r({'courseopt':re})
